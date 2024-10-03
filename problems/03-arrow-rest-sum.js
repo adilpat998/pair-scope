b75e3b0f32408e9dc112f55590bffe4b3d3d7747
adilpat998 <adilpathan541@gmail.com>
2024-10-03 17:08:19 -0400

    3

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,7 +11,25 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
+const arrowRestSum = (...all) => 
+{let sum = 0
+  return all.map((num) => {
+    sum += num
+    return sum 
+  })
+ 
+}
+
+console.log(arrowRestSum(3,5,6));
+
+
+
+
+
+
+
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -5,12 +5,25 @@ variable using the const keyword.
 
 Examples:
 
-arrowAvgValue([10, 20]); // => 15
+; // => 15
 arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// Your code here 
+const func =  arrowAvgValue = (arr) => {
+let sum = 0 
+for (let num of arr){
+  sum += num
+}
+return sum / arr.length
+}
+console.log((func)([10, 20]))
+
+
+
+
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
