87c302d6e97048e91a8d1bd2a287bcd2655e15e7
tysonmcdowell <tyson.mcdowell06@gmail.com>
2024-10-03 17:50:02 -0500

    10

--- problems/08-hidden-counter.js
+++ problems/08-hidden-counter.js
@@ -5,17 +5,27 @@ When invoked the function returned by hiddenCounter will increment the counter b
 
 Look below to see how this function is invoked:
 
-let hidden1 = hiddenCounter(); //returns a function
-hidden1(); // returns 1
-hidden1(); // returns 2
 
-let hidden2 = hiddenCounter(); // returns a function
-hidden2(); // returns 1
 
 
 ***********************************************************************/
 
-// Your code here 
+function hiddenCounter() {
+  let count = 0; // Initialize the count variable
+
+  return function increment() {
+    count++; // Increment the count
+    return count; // Return the updated count
+  }
+}
+
+ let hidden1 = hiddenCounter(); //returns a function
+console.log(hidden1()); // returns 1
+console.log(hidden1()); // returns 2
+
+
+let hidden2 = hiddenCounter(); // returns a function
+console.log(hidden2()); // returns 1
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -4,9 +4,7 @@ accept a word and return a function. When the function returned by interrupter
 is invoked with a string the string will be returned with "interruptions".
 
 Look below to see how this function is invoked:
-let rudePerson = interrupter("what"); // => returns a function
-console.log(rudePerson("how are you")); // prints "how what are what you"
-console.log(rudePerson("I like pie")); // prints "I what like what pie"
+
 
 
 Invoking the interrupter function again:
@@ -16,8 +14,15 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 ***********************************************************************/
 
-// Your code here 
+function interrupter(interruptingWord) {
+  return function(sentence) {
+    return sentence.split(' ').join(` ${interruptingWord} `);
+  }
+}
 
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
