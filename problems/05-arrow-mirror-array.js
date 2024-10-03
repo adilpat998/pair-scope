e83029e3b460ed02b1652832b9443c4c5e7d6352
tysonmcdowell <tyson.mcdowell06@gmail.com>
2024-10-03 17:01:48 -0500

    5

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -6,12 +6,20 @@ Write this function using an arrow function!
 
 Examples:
 
-arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+a
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// Your code here 
+//  let arrowMirrorArray = (arr) => {
+//   const newArr = arr.slice()
+//   return arr.concat(newArr);
+//  };
 
+ const arrowMirrorArray = (arr) => {
+  return [...arr.slice(), ...arr.reverse()];
+};
+
+ console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
